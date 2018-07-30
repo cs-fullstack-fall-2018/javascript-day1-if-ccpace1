9f5e57f35df6ac8bf6f839225dedfb958c0304fa
Christopher Pace <Christopher_pace@ymail.com>
2018-07-30 14:40:39 -0500

    Add 7&8

---
+++ exercise7.js
@@ -0,0 +1,11 @@
+var number1 = parseInt(prompt("Please enter a number: "));
+
+if (number1 >0){
+    alert("Number is positive")
+}
+else if (number1 <0) {
+    alert("Number is negative");
+}
+else {
+    alert("Number is zero")
+}
---
+++ exercise8.js
@@ -0,0 +1,8 @@
+var number1 = parseInt(prompt("Please enter a number: "));
+
+if ((number1 > 0) && number1 <3){
+    alert("Correct!")
+    }
+else{
+        alert("Error")
+}
